1926a40db97a84062991923dcf9100a5e50d4018
Mário Lino Salembe <linomario199010@gmail.com>
2024-05-16 00:37:38 +0100

    added some projects

--- libft.h
+++ libft.h
@@ -13,8 +13,7 @@
 #ifndef LIBFT_H
 
 # define LIBFT_H
-# include "unistd.h"
-# include <stdio.h>
+# include <unistd.h>
 # include <stdlib.h>
 
 int		ft_isalpha(int c);
@@ -31,10 +30,12 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
+void    *ft_calloc(size_t nmemb, size_t size);
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
