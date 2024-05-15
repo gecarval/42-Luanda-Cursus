0414a18f7a20469623c10a001138c5cd802e06ca
Mário Lino Salembe <linomario199010@gmail.com>
2024-05-16 00:46:56 +0100

    added ft_strdup

--- libft.h
+++ libft.h
@@ -31,6 +31,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
+char    *ft_strdup(const char *s);
 
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
